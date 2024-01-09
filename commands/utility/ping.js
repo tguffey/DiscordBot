c93458d25c7421b1fbe8afa1387e0d076ce805be
Stephen Everett <alex.alexeverett@gmail.com>
2024-01-08 18:21:16 -0800

    extended functionality of ping command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -5,7 +5,15 @@ module.exports = {
         .setName('ping')
         .setDescription('Replies with Pong!'),
     async execute(interaction) {
+        if (interaction.user.id === "416592169780903937") {
+            await interaction.reply(":bangbang: LITTLE DOG DETECTED :bangbang: LITTLE DOG DETECTED :bangbang:")
+        }
+        else if (interaction.user.id === "300050440672903168") {
+            await interaction.reply("What's up?")
+        }
+        else {
+            await interaction.reply('Pong!');
+        }
         console.log("/ping command used")
-        await interaction.reply('Pong!');
     },
 };
